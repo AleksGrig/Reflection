fb5d913b78393173cf83c79474afa64f7ec81fee
Aleks Grig <ullitap@gmail.com>
2020-03-15 12:12:25 +0300

    Jar + dependency for class loading tests

--- Reflection/src/com/generator/SuperPassword.java
+++ Reflection/src/com/generator/SuperPassword.java
@@ -1,7 +1,12 @@
 package com.generator;
 
+import enums.Book;
+
 // Represents third-party class
 public class SuperPassword {
+
+	// dummy dependency for class loading tests
+	public Book book = new Book();
 
 	@Generator
 	public String createPassword() {
